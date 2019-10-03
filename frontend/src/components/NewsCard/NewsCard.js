9c8d99ece064f15ac6e3d3e0be93bc7c85c5326b
romazrau <b04208017@ntu.edu.tw>
2019-10-03 21:00:11 +0800

    News card group OK

--- frontend/src/components/NewsCard/NewsCard.js
+++ frontend/src/components/NewsCard/NewsCard.js
@@ -3,17 +3,17 @@ import {
     Card
 } from "react-bootstrap"; //, NavDropdown
 
-export default ({srcRequire , text})=>{
+export default ({srcRequire , text,onclick})=>{
     return (
-        <Card>
+        <Card href="#52" onClick={onclick}>
           <Card.Img variant="top" src={srcRequire} />
-          <Card.Body>
+          <Card.Body style={{ backgroundColor:"rgb(204, 204, 204)"}}>
             <Card.Text>
               {text}
             </Card.Text>
           </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+          <Card.Footer style={{ height:"1vh" , padding:"0 0 0 0" ,backgroundColor:"rgb(255, 189, 6)"}}>
+            {/* <small className="text-muted">Last updated 3 mins ago</small> */}
           </Card.Footer>
         </Card>
     )
--- frontend/src/containers/NewsCardGroup/NewsCardGroup.js
+++ frontend/src/containers/NewsCardGroup/NewsCardGroup.js
@@ -2,30 +2,51 @@ import React from "react";
 import { CardDeck } from "react-bootstrap"; //, NavDropdown
 
 import NewsCard from "../../components/NewsCard/NewsCard";
-import classes from "./NewsCardGroup.module.css"
+import classes from "./NewsCardGroup.module.css";
 
 class NewsCardGroup extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          news: [
-              ["bear.jpg","This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."],
-              ["bear.jpg","This card has supporting text below as a natural lead-in to additional content."],
-              ["bear.jpg","This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action."]
-          ]
-        };
-      }
+  constructor(props) {
+    super(props);
+    this.state = { //  "新聞的照片(放在img裡的名稱) + 跳轉URL  + 介紹 " *3
+      news: [
+        [
+          "bear.jpg",
+          "#50",
+          "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+        ],
+        [
+          "bear.jpg",
+          "#60",
+          "This card has supporting text below as a natural lead-in to additional content."
+        ],
+        [
+          "bear.jpg",
+          "https://www.facebook.com/",
+          "This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action."
+        ]
+      ]
+    };
+  }
+
+  newsCardClick = url => {
+    console.log("newsCard clicked. URL is " + url)
+    window.location.href=url;
+    // this.props.history.push("#info");
+  };
 
   render() {
-    let display = this.state.news.map(
-        e => (
-            <NewsCard srcRequire={require("../../img/"+e[0])} text={e[1]} key={e}></NewsCard>
-        )
-    )
+    let display = this.state.news.map(e => (
+      <NewsCard
+        srcRequire={require("../../img/" + e[0])}
+        onclick={() => this.newsCardClick(e[1])}
+        text={e[2]}
+        key={e}
+      ></NewsCard>
+    ));
     return (
-      <CardDeck className={classes.cardDeck}>
-        {display}
-      </CardDeck>
+      <div className={classes.cardDeck}>
+        <CardDeck>{display}</CardDeck>
+      </div>
     );
   }
 }
